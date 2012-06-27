a6b4939dcfca0961b785dcd4d6b72521ac5bf76e
Brian Ledworth <brian@ledsworth.com>
2012-06-27 13:50:56 -0500

    Build gem 0.0.6

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "random_fu's version" do
   it "should be current" do
-    RandomFu::VERSION.should == '0.0.5'
+    RandomFu::VERSION.should == '0.0.6'
   end
 end
 
